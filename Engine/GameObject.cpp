967f289e319729dd77023d08572085be2cd0f63e
Park in su <eyiuta1@gmail.com>
2025-07-07 18:54:58 +0900

    Rigidbody 충돌 구현 완료.

--- Engine/GameObject.cpp
+++ Engine/GameObject.cpp
@@ -221,7 +221,7 @@ void GameObject::AddComponent(shared_ptr<Component> _component)
 //충돌 관련 함수. 
 void GameObject::OnCollision(shared_ptr<GameObject> _other)
 {
-	if (GetRigidbody() != nullptr && _other->GetRigidbody() == nullptr) {
+	if (GetRigidbody() != nullptr) {
 		GetRigidbody()->OnCollision(_other);
 	}
 }
