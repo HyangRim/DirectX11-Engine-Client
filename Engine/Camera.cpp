d9daa2d6baacb570dac8822e7ee146bae327aa03
Park in su <eyiuta1@gmail.com>
2025-07-15 23:49:47 +0900

    병합 에러 해결.

--- Engine/Camera.cpp
+++ Engine/Camera.cpp
@@ -58,131 +58,132 @@ void Camera::UpdateMatrix()
 
 void Camera::SortGameObject()
 {
-	shared_ptr<Scene> scene = CURSCENE;
-	const unordered_set<shared_ptr<GameObject>>& gameObjects = scene->GetObjects();
+    shared_ptr<Scene> scene = CURSCENE;
+    const unordered_set<shared_ptr<GameObject>>& gameObjects = scene->GetObjects();
+    const unordered_set<shared_ptr<GameObject>>& uiObjects = scene->m_uiObjects;
 
-	m_vecForward.clear();
-	m_vecBackward.clear();
+    m_vecForward.clear();
+    m_vecBackward.clear();
 
-	// FOW 인터페이스 캐싱 시스템 (성능 최적화)
-	static IFogOfWar* cachedFogOfWar = nullptr;
-	static int lastFrameCheck = -1;
-	int currentFrame = GetTickCount64() / 16; // 60FPS 기준
+    // FOW 인터페이스 캐싱 시스템 (성능 최적화)
+    static IFogOfWar* cachedFogOfWar = nullptr;
+    static int lastFrameCheck = -1;
+    int currentFrame = GetTickCount64() / 16; // 60FPS 기준
 
-	if (lastFrameCheck != currentFrame) {
-		cachedFogOfWar = nullptr;
+    if (lastFrameCheck != currentFrame) {
+        cachedFogOfWar = nullptr;
 
-		// IFogOfWar 인터페이스 구현체 찾기
-		for (auto& obj : gameObjects) {
-			auto scripts = obj->GetScripts();
-			for (auto& comp : scripts) {
-				IFogOfWar* fogInterface = dynamic_cast<IFogOfWar*>(comp.get());
-				if (fogInterface) {
-					cachedFogOfWar = fogInterface;
-					break;
-				}
-			}
-			if (cachedFogOfWar) break;
-		}
-		lastFrameCheck = currentFrame;
-	}
+        // IFogOfWar 인터페이스 구현체 찾기
+        for (auto& obj : gameObjects) {
+            auto scripts = obj->GetScripts();
+            for (auto& comp : scripts) {
+                IFogOfWar* fogInterface = dynamic_cast<IFogOfWar*>(comp.get());
+                if (fogInterface) {
+                    cachedFogOfWar = fogInterface;
+                    break;
+                }
+            }
+            if (cachedFogOfWar) break;
+        }
+        lastFrameCheck = currentFrame;
+    }
 
-	// FOW 시스템 업데이트 (엔진에서 호출)
-	if (cachedFogOfWar) {
-		cachedFogOfWar->UpdateFOWSystem();
-	}
+    // FOW 시스템 업데이트 (엔진에서 호출)
+    if (cachedFogOfWar) {
+        cachedFogOfWar->UpdateFOWSystem();
+    }
 
-	if (m_type == ProjectionType::Perspective) 
-	{
-		int CullingObject = 0;
-		
-	
-		//cout << "전체 오브젝트 : " << gameObjects.size() << "\n";
-		//그려줄 것 선별하기. 
-		for (auto& object : gameObjects) 
-		{
-			if (object->GetType() != OBJECTTYPE::MAP) {
-				//레이어 컬링. 
-				if (IsCulled(object->GetLayerIndex()))
-					continue;
-
-				// QuadTree를 통한 Frustum Culling.
-				if (!scene->GetQuadTree()->IsObjectVisible(object, this))
-				{
-					CullingObject++;
-					continue;
-				}
-
-				//FOW통한 컬링. 
-				if (cachedFogOfWar) {
-					if (!cachedFogOfWar->ShouldRenderObject(object)) {
-						continue;
-					}
-
-					float alpha = cachedFogOfWar->GetObjectAlpha(object);
-					object->SetAlpha(alpha);
-				}
-			}
+    if (m_type == ProjectionType::Perspective)
+    {
+        int CullingObject = 0;
 
 
-			//QuadTree - Visible가지고 Frustum Culling 가능. 
-			shared_ptr<Renderer> renderer = object->GetRenderer();
-			if (renderer == nullptr)
-				continue;
+        //cout << "전체 오브젝트 : " << gameObjects.size() << "\n";
+        //그려줄 것 선별하기. 
+        for (auto& object : gameObjects)
+        {
+            if (object->GetType() != OBJECTTYPE::MAP) {
+                //레이어 컬링. 
+                if (IsCulled(object->GetLayerIndex()))
+                    continue;
 
-			shared_ptr<Material> material = renderer->GetMaterial();
-			RenderQueue renderQueue = material->GetRenderQueue();
+                // QuadTree를 통한 Frustum Culling.
+                if (!scene->GetQuadTree()->IsObjectVisible(object, this))
+                {
+                    CullingObject++;
+                    continue;
+                }
 
-			//TODO : 컷아웃용 정렬하기
-			//TODO : 거리에 따라 정렬하기
+                //FOW통한 컬링. 
+                if (cachedFogOfWar) {
+                    if (!cachedFogOfWar->ShouldRenderObject(object)) {
+                        continue;
+                    }
 
-			switch (renderQueue)
-			{
-				case RenderQueue::Opaque:
-				case RenderQueue::Cutout:
-					m_vecForward.push_back(object);
-					break;
-				case RenderQueue::Transparent:
-					m_vecBackward.push_back(object);
-					break;
+                    float alpha = cachedFogOfWar->GetObjectAlpha(object);
+                    object->SetAlpha(alpha);
+                }
+            }
 
-			}
 
-			//cout << "컬링 오브젝트 : " << CullingObject << "\n";
-			//cout << "렌더링 오브젝트 : " << gameObjects.size() - CullingObject << "\n";
-		}
-	}
-	else
-	{
-		//그려줄 것 선별하기. 
-		for (auto& object : gameObjects) 
-		{
-			if (IsCulled(object->GetLayerIndex()))
-				continue;
+            //QuadTree - Visible가지고 Frustum Culling 가능. 
+            shared_ptr<Renderer> renderer = object->GetRenderer();
+            if (renderer == nullptr)
+                continue;
 
-			//QuadTree - Visible가지고 Frustum Culling 가능. 
-			shared_ptr<Renderer> renderer = object->GetRenderer();
-			if (renderer == nullptr)
-				continue;
+            shared_ptr<Material> material = renderer->GetMaterial();
+            RenderQueue renderQueue = material->GetRenderQueue();
 
-			shared_ptr<Material> material = renderer->GetMaterial();
-			
-			RenderQueue renderQueue = material->GetRenderQueue();
+            //TODO : 컷아웃용 정렬하기
+            //TODO : 거리에 따라 정렬하기
 
-			//TODO : 컷아웃용 정렬하기
-			//TODO : 거리에 따라 정렬하기
-			switch (renderQueue)
-			{
-				case RenderQueue::Opaque:
-				case RenderQueue::Cutout:
-					m_vecForward.push_back(object);
-					break;
-				case RenderQueue::Transparent:
-					m_vecBackward.push_back(object);
-					break;
-			}
-		}
-	}
+            switch (renderQueue)
+            {
+            case RenderQueue::Opaque:
+            case RenderQueue::Cutout:
+                m_vecForward.push_back(object);
+                break;
+            case RenderQueue::Transparent:
+                m_vecBackward.push_back(object);
+                break;
+
+            }
+
+            //cout << "컬링 오브젝트 : " << CullingObject << "\n";
+            //cout << "렌더링 오브젝트 : " << gameObjects.size() - CullingObject << "\n";
+        }
+    }
+    else
+    {
+        //그려줄 것 선별하기. 
+        for (auto& object : uiObjects)
+        {
+            if (IsCulled(object->GetLayerIndex()))
+                continue;
+
+            //QuadTree - Visible가지고 Frustum Culling 가능. 
+            shared_ptr<Renderer> renderer = object->GetRenderer();
+            if (renderer == nullptr)
+                continue;
+
+            shared_ptr<Material> material = renderer->GetMaterial();
+
+            RenderQueue renderQueue = material->GetRenderQueue();
+
+            //TODO : 컷아웃용 정렬하기
+            //TODO : 거리에 따라 정렬하기
+            switch (renderQueue)
+            {
+            case RenderQueue::Opaque:
+            case RenderQueue::Cutout:
+                m_vecForward.push_back(object);
+                break;
+            case RenderQueue::Transparent:
+                m_vecBackward.push_back(object);
+                break;
+            }
+        }
+    }
 }
 
 void Camera::SetStaticData() {
