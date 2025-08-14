99eb2004fb857bbb16746a86f0e9d1214016a815
vfly1189 <vfly1189@naver.com>
2025-08-14 17:08:15 +0900

    병합용

---
+++ Client/ItemManager.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+#include "ItemManager.h"
+
+ItemManager::ItemManager()
+	:Super(ComponentType::Custom)
+{
+
+}
+
+ItemManager::~ItemManager()
+{
+
+}
---
+++ Client/ItemManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Component.h"
+class ItemManager :
+    public Component
+{
+    using Super = Component;
+public:
+    ItemManager();
+    ~ItemManager();
+
+
+
+
+};
+
